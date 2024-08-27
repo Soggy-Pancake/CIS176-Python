b4a11a2ba99c136fc984aa3f101ba29c19031305
Soggy_Pancake <no@goaway.com>
2024-08-26 18:28:58 -0700

    Initialised repo with first assignment!

---
+++ Assignments/Ch1_PE1/__init__.py
@@ -0,0 +1,20 @@
+#print("Loading Chapter 1 Exersize 1!")
+
+def fullAssignmentName() -> str:
+    return "Chapter 1 Exersize 1"
+
+def main():
+    print("Hello, Mike!")
+    print("Hello", "Mike!")
+    print(5)
+    print(5.0)
+    print(2 + 5)
+    print(2.0 + 5.0)
+    print("2" + "5")
+    print("2 + 5 = ", 2 + 5)
+    print(2 * 5)
+    print(5 ** 3)
+    print(11 / 3)
+    print(11 // 3)
+    # input used to keep print statements on screen 
+    input("Finished Running...")
---
+++ main.py
@@ -0,0 +1,53 @@
+import os, sys, importlib
+
+print("Importing assignments!")
+
+assignmentModules = []
+try:
+    sys.path.insert(0, os.path.join(__file__[:__file__.rfind("\\")], "Assignments") )
+except:
+    print("Failed to add assignments folder to path! Exiting...")
+    sys.exit()
+
+for assignment in os.listdir("Assignments"):
+    #print(f"Assignment {assignment}")
+    #from Assignments import Ch1Ex1
+    try:
+        assignmentModules.append(importlib.import_module(assignment))
+    except Exception as e:
+        print(f"Import FAIL for assignment {assignment} : {e}")
+
+def printMenu() -> None:
+    for i in range(len(assignmentModules)):
+        print(f"{i + 1}. {assignmentModules[i].fullAssignmentName()}")
+
+print(assignmentModules)
+
+while True:
+    os.system("cls")
+    printMenu()
+    print("0. Exit")
+    try:
+        selected = input("Input Assignment: ")
+
+        if selected == "0":
+            sys.exit()
+        if selected == "":
+            raise ValueError
+            #Just to warn that im looking for a number
+        
+        selected = int(selected)
+
+        if selected > len(assignmentModules):
+            raise ModuleNotFoundError
+        else:
+            # Run assignment :3 
+            assignmentModules[selected - 1].main()
+
+
+    except Exception as e:
+        print(e, type(e))
+        if type(e) == ModuleNotFoundError:
+            print("Assignment not found!")
+        elif type(e) == ValueError:
+            print('Please input number before the assignment name! (Ex with "1. Assignment" input the number 1)')
